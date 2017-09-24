e8a11cfcef357e6156f3ce7efea51a58a2d0cbc6
石丸 直宏 <naohiro_ishimaru@ricoh-japan.co.jp>
2017-09-24 21:57:23 +0900

    アンチパターン追加

---
+++ ConsoleApp1/ConsoleApp2/ConcreteClass2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class ConcreteClass2 : AbstractClass
+    {
+        protected override void ExecuteInnerProc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
